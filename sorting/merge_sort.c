8d841cafc3f80695bf738a13db06fe773a2e6c36
Hance08 <hancechin08@gmail.com>
2025-10-04 12:45:16 +0800

    Add some comments in merge_sort.c

--- sorting/merge_sort.c
+++ sorting/merge_sort.c
@@ -20,54 +20,54 @@ int main()
     return 0;
 }
 
-void mergeSort(int arr[], int left, int right) // Example left = 0, right = 2 [3, 5, 1]
+void mergeSort(int arr[], int left, int right)
 {
     if (left < right)
     {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);      // Divide to left array
-        mergeSort(arr, mid + 1, right); // Divide to right array
-        merge(arr, left, mid, right);   // When the recursion is fullfill, start merge
+        int mid = left + (right - left) / 2; // Find the mid index, equal to (left + right) / 2, but more safety
+        mergeSort(arr, left, mid);           // Divide to left array
+        mergeSort(arr, mid + 1, right);      // Divide to right array
+        merge(arr, left, mid, right);        // After the above recursion function finished, start merging
     }
 }
 
-void merge(int arr[], int left, int mid, int right) // First execute argument (arr, 0, 0, 1)
+void merge(int arr[], int left, int mid, int right)
 {
-    int left_arr_len = mid - left + 1;
-    int right_arr_len = right - mid;
+    int left_arr_len = mid - left + 1; // Define the length of left array
+    int right_arr_len = right - left;  // Define the length of right array
 
-    int L[left_arr_len], R[right_arr_len];
-
-    for (int i = 0; i < left_arr_len; i++)
+    int L[left_arr_len], R[right_arr_len]; // Create the temp arrays for left and right
+    for (int i = 0; i < left_arr_len; i++) // Fill the value from origin array to temp array
         L[i] = arr[left + i];
-    for (int j = 0; j < right_arr_len; j++)
+    for (int j = 0; j < right_arr_len; j++) // Fill the value from origin array to temp array
         R[j] = arr[mid + 1 + j];
 
-    int i = 0, j = 0, key = left;
-    while (i < left_arr_len && j < right_arr_len)
+    int i = 0, j = 0, insert_index = left;        // Create the index for comparing the vaule between left array and right array
+    while (i < left_arr_len && j < right_arr_len) // Check if either left or right array is empty
     {
-        if (L[i] <= R[j])
+        if (L[i] <= R[j]) // If left array's value less equal than right arrays's vaule
         {
-            arr[key] = L[i];
-            i++;
+            arr[insert_index] = L[i]; // Put the left array's value into the origin array
+            i++;                      // Move one index forward from left array
         }
         else
         {
-            arr[key] = R[j];
-            j++;
+            arr[insert_index] = R[j]; // Put the right array's value into the origin array
+            j++;                      // Move one index forward from right array
         }
-        key++;
-    }
-    while (i < left_arr_len)
-    {
-        arr[key] = L[i];
-        i++;
+        insert_index++; // Move one index forward from origin array
     }
 
+    while (i < left_arr_len) // This statement will be used while there have remained value in the left array and then put all the remain value in the origin array
+    {
+        arr[insert_index] = L[i];
+        i++;
+        insert_index++;
+    }
     while (j < right_arr_len)
     {
-        arr[key] = R[j];
+        arr[insert_index] = R[j];
         j++;
+        insert_index++;
     }
 }
